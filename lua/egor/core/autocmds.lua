424e22af412682d9f8139245a5d65106c2b6b0fa
Egor Chernakov <chernakov.eg@gmail.com>
2024-01-18 16:37:49 +0300

    add auto cplint & black

---
+++ lua/egor/core/autocmds.lua
@@ -0,0 +1,25 @@
+-- Create group to assign commands
+-- "clear = true" must be set to prevent loading an
+-- auto-command repeatedly every time a file is resourced
+local autocmd_group = vim.api.nvim_create_augroup("Custom auto-commands", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    pattern = { "*.cpp", "*.c", "*.h" },
+    desc = "Auto-format C and C++ files after saving",
+    callback = function()
+        local fileName = vim.api.nvim_buf_get_name(0)
+        vim.cmd(":silent !clang-format --style=Google -i " .. fileName)
+    end,
+    group = autocmd_group,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    pattern = { "*.py"},
+    desc = "Auto-format python files after saving",
+    callback = function()
+        local fileName = vim.api.nvim_buf_get_name(0)
+        vim.cmd(":silent !black " .. fileName)
+    end,
+    group = autocmd_group,
+})
+
