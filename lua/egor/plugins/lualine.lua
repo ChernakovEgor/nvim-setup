91f5d65850caea4e23a9afe2101813c78975fb9a
Chernakov Egor <chernakov.eg@gmail.com>
2026-02-02 14:36:31 +0300

    update lualine

--- lua/egor/plugins/lualine.lua
+++ lua/egor/plugins/lualine.lua
@@ -31,7 +31,7 @@ require('lualine').setup {
     },
 
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path=1}},
     lualine_x = {'filetype'},
     lualine_y = {'searchcount'},
     lualine_z = {
@@ -41,7 +41,7 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path=1}},
     lualine_x = {'filetype'},
     lualine_y = {'location'},
     lualine_z = {}
